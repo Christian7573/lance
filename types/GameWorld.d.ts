8d970c731f14e7e26a8e0e1e2a60ee175613bdae
Christian7573 <cag4561@gmail.com>
2019-06-03 14:37:32 -0500

    Finished standard port of GameWorld file

--- types/GameWorld.d.ts
+++ types/GameWorld.d.ts
@@ -1,3 +1,17 @@
+
+export interface GameWorldQuery {
+    /** Object id */
+    id?: number;
+    /** Player id */
+    playerId?: number;
+    /** Instance type */
+    instanceType: { new (...args): any }
+    /** An array of component names */
+    componentNames: string[];
+    /** Only return a single object. Default: false */
+    returnSingle: boolean;
+}
+
 /**
  * This class represents an instance of the game world,
  * where all data pertaining to the current state of the
@@ -19,6 +33,42 @@ export class GameWorld {
      * @return {Number} the new id
      */
     getNewId(): number;
+
+    /**
+     * Returns all the game world objects which match a criteria
+     * @param {GameWorldQuery} query The query object
+     * @returns {Array | GameObject} All game objects which match all the query parameters, or the first match if returnSingle was specified
+     */
+    queryObjects(query: GameWorldQuery & { returnSingle: false }): GameObject[];
+    queryObjects(query: GameWorldQuery & { returnSingle: true }): GameObject;
+
+    /**
+     * Returns The first game object encountered which matches a criteria.
+     * Syntactic sugar for {@link queryObjects} with `returnSingle: true`
+     * @param {GameWorldQuery} query The query object
+     * @returns {GameObject}
+     */
+    queryObject(query: GameWorldQuery): GameObject;
+    
+    /**
+     * Add an object to the game world
+     * @param {GameObject} object object to add
+     */
+    addObject(object: GameObject): void;
+
+    /**
+     * Remove an object from the game world
+     * @param {number} id id of the object to remove
+     */
+    removeObject(id: number): void;
+
+    /**
+     * World object iterator.
+     * Invoke callback(objId, obj) for each object
+     *
+     * @param {function} callback function receives id and object. If callback returns false, the iteration will cease
+     */
+    forEachObject(callback: (number, GameObject) => any): void;
 }
 
 export default GameWorld;
