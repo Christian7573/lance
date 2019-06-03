dc99cf01a86f56b7a1bfd05fdb89f2cd49c3fb5d
Christian7573 <cag4561@gmail.com>
2019-06-03 11:09:58 -0500

    Started owrk on GameWorld.d.ts

---
+++ types/GameWorld.d.ts
@@ -0,0 +1,26 @@
+declare namespace LanceGG {
+    /**
+     * This class represents an instance of the game world,
+     * where all data pertaining to the current state of the
+     * world is saved.
+     */
+    export class GameWorld {
+        stepCount: number;
+        objects: object;
+        playerCount: number;
+        idCount: number;
+
+        /**
+         * Constructor of the World instance
+         */
+        constructor();
+        
+        /**
+         * Gets a new, fresh and unused id that can be used for a new object
+         * @return {Number} the new id
+         */
+        getNewId(): number;
+    }
+}
+
+export default LanceGG.GameWorld;
