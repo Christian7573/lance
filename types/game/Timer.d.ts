480f34d21d0b37ed714ebc4a78877a157e1cb1d8
Christian7573 <cag4561@gmail.com>
2019-06-05 15:54:10 -0500

    Finished Timer.d.ts

--- types/game/Timer.d.ts
+++ types/game/Timer.d.ts
@@ -14,7 +14,25 @@ export default class Timer {
 
     loop(time: number, callback: Function): TimerEvent;
 
-    add(time: number, callback: Function, thisContext: any, args: any[]): TimerEvent;]
+    add(time: number, callback: Function, thisContext: any, args: any[]): TimerEvent;
 
     destroy(id: number);
+}
+
+export type TimerEventType = "repeat" | "single";
+export class TimerEvent {
+    constructor(timer: Timer, type: string, time: TimerEventType, callback: Function, thisContext: any, args: any[]);
+
+    id: number;
+    timer: Timer;
+    type: TimerEventType;
+    time: number;
+    callback: Function;
+    startOffset: number;
+    thisContext: any;
+    args: any[];
+
+    destroy();
+
+    static TYPES: { repeat: "repeat", single: "single" }
 }
