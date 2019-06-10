2de0489e4a679015798860e98e7a2f3a517e8859
Christian7573 <cag4561@gmail.com>
2019-06-10 10:15:20 -0500

    HSHG

---
+++ types/physics/SimplePhysics/HSHG.d.ts
@@ -0,0 +1,23 @@
+import { DynamicObject } from "../../package/clientExports";
+
+declare interface typeof_hshg {
+    new (): instanceof_hshg;
+    update_RECOMPUTE: () => void;
+    update_REMOVEALL: () => void;
+}
+declare interface instanceof_hshg {
+    addObject(obj: DynamicObject): void;
+    removeObject(obj: DynamicObject): void;
+    update(): void;
+    queryForCollisionPairs(broadOverlapTestCallback: (objA: DynamicObject, objB: DynamicObject) => boolean): [DynamicObject, DynamicObject][];
+    MAX_OBJECT_CELL_DENSITY: number;
+    /** Default: 256 = 16x16 */
+    INITIAL_GRID_LENGTH: number;
+    HIERARCHY_FACTOR: number;
+    HIERARCHY_FACTOR_SQRT: number;
+    UPDATE_METHOD: () => void;
+}
+
+declare const HSHG: typeof_hshg;
+
+export { HSHG as default };
