530698ec587eb5f94c4538ca2856963e5f2dd3cb
Christian7573 <cag4561@gmail.com>
2019-06-11 08:08:49 -0500

    Adjusting exports for brawl

--- src/package/clientExports.js
+++ src/package/clientExports.js
@@ -2,14 +2,12 @@ import GameEngine from '../GameEngine.js';
 import GameWorld from '../GameWorld.js';
 import P2PhysicsEngine from '../physics/P2PhysicsEngine.js';
 import SimplePhysicsEngine from '../physics/SimplePhysicsEngine.js';
-import CannonPhysicsEngine from '../physics/CannonPhysicsEngine.js';
 import BaseTypes from '../serialize/BaseTypes.js';
 import TwoVector from '../serialize/TwoVector.js';
 import ThreeVector from '../serialize/ThreeVector.js';
 import Quaternion from '../serialize/Quaternion.js';
 import DynamicObject from '../serialize/DynamicObject.js';
 import PhysicalObject2D from '../serialize/PhysicalObject2D.js';
-import PhysicalObject3D from '../serialize/PhysicalObject3D.js';
 import Lib from '../lib/lib.js';
 import ClientEngine from '../ClientEngine.js';
 import KeyboardControls from '../controls/KeyboardControls.js';
@@ -21,14 +19,12 @@ export {
   GameWorld,
   P2PhysicsEngine,
   SimplePhysicsEngine,
-  CannonPhysicsEngine,
   BaseTypes,
   TwoVector,
   ThreeVector,
   Quaternion,
   DynamicObject,
   PhysicalObject2D,
-  PhysicalObject3D,
   Lib,
   ClientEngine,
   KeyboardControls,
--- src/package/serverExports.js
+++ src/package/serverExports.js
@@ -2,14 +2,12 @@ import GameEngine from '../GameEngine.js';
 import GameWorld from '../GameWorld.js';
 import P2PhysicsEngine from '../physics/P2PhysicsEngine.js';
 import SimplePhysicsEngine from '../physics/SimplePhysicsEngine.js';
-import CannonPhysicsEngine from '../physics/CannonPhysicsEngine.js';
 import BaseTypes from '../serialize/BaseTypes.js';
 import TwoVector from '../serialize/TwoVector.js';
 import ThreeVector from '../serialize/ThreeVector.js';
 import Quaternion from '../serialize/Quaternion.js';
 import DynamicObject from '../serialize/DynamicObject.js';
 import PhysicalObject2D from '../serialize/PhysicalObject2D.js';
-import PhysicalObject3D from '../serialize/PhysicalObject3D.js';
 import ServerEngine from '../ServerEngine.js';
 import Lib from '../lib/lib.js';
 
@@ -18,14 +16,12 @@ export {
   GameWorld,
   P2PhysicsEngine,
   SimplePhysicsEngine,
-  CannonPhysicsEngine,
   BaseTypes,
   TwoVector,
   ThreeVector,
   Quaternion,
   DynamicObject,
   PhysicalObject2D,
-  PhysicalObject3D,
   ServerEngine,
   Lib,
 };
